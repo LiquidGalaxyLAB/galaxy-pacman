dddc86c08e5e98f7a94bc4af1701f83c3c79782b
leoruas <leo.coelho.ruas@gmail.com>
2021-06-20 16:04:02 -0300

    added map builder

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-//server config
+// server initialization
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
-app.use(express.static(__dirname + '/public'))
+const port = 8128;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+// setup files to be sent on connection
+const filePath = "/public" // do not add '/' at the end
+const gameFile = "index.html"
+const mapBuilderFile = "mapbuilder/index.html"
+
+
+app.use(express.static(__dirname + filePath))
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id
+    if(id == 'mapbuilder') {
+        res.sendFile(__dirname + `${filePath}/${mapBuilderFile}`);
+    } else {
+        res.sendFile(__dirname + `${filePath}/${gameFile}`);
+    }
 })
 
-const port = 8128;
 http.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
---
+++ public/mapbuilder/main.js
@@ -0,0 +1,57 @@
+import {GRID_WIDTH, GRID_HEIGHT, BLOCK_SIZE} from "../consts.js"
+
+const grid = document.getElementById('grid')
+const button = document.getElementById('button')
+const classes = [
+    {name: "wall", value: 1},
+    {name: "food", value: 2},
+    {name: "powerup", value: 4},
+    {name: "blank", value: 0},
+]
+
+function main() {
+    button.addEventListener('click', print)
+
+    grid.style.gridTemplateColumns = `repeat(${GRID_WIDTH}, ${BLOCK_SIZE}px)`
+
+    for(let i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) {
+        const block = document.createElement('div')
+        block.style.minHeight = `${BLOCK_SIZE}px`
+        block.style.minWidth = `${BLOCK_SIZE}px`
+        block.className = "wall"
+        block.addEventListener('click', updateBlock)
+        grid.appendChild(block)
+    }
+}
+
+function updateBlock(ev) {
+    const className = ev.target.className
+    const index = classes.findIndex(c => c.name == className)
+    if(index == classes.length - 1) {
+        ev.target.className = classes[0].name
+    } else {
+        ev.target.className = classes[index + 1].name
+    }
+}
+
+function print() {
+    let final = "["
+    Array.from(grid.children).forEach(child => {
+        let row = 0
+        let col = 0
+        let value = classes.find(c => c.name == child.className).value
+        final = final.concat(`${value}, `)
+        
+        if(col == GRID_WIDTH - 1) {
+            row++
+            col = 0
+        } else {
+            col++
+        }
+    })
+    final = final.slice(0, -2)
+    final = final.concat(']')
+    console.log('map =', final)
+}
+
+main()
