c8ca7b37008676498ce72649b62150a6051774a9
leoruas <leo.coelho.ruas@gmail.com>
2021-06-30 21:46:51 -0300

    updated ghost drawing

--- public/models/Ghost.js
+++ public/models/Ghost.js
@@ -23,15 +23,47 @@ class Ghost {
         ctx.setTransform(1, 0, 0, 1, 0, 0); //reset transform before drawing
         ctx.fillStyle = this.color;
 
-        let radius = this.size / 2
+        const headRadius = this.size / 2
 
         // draw cricle
         ctx.beginPath();
-        ctx.arc(this.x + radius, this.y + radius, radius, 0, 2 * Math.PI);
+        ctx.arc(this.x + headRadius, this.y + headRadius, headRadius, 0, 2 * Math.PI);
         ctx.fill()
 
-        //draw rectangle on base
-        ctx.fillRect(this.x, this.y + radius, this.size, radius)
+        //draw rectangle for body
+        ctx.fillRect(this.x, this.y + headRadius, this.size, headRadius)
+
+        //draw little circles for legs
+        const nLegs = 3 // number of legs
+        const legRadius = headRadius / nLegs
+        ctx.beginPath();
+        for(let i = 0; i < nLegs; i++) {
+            ctx.arc(this.x + ((2 * i + 1) * legRadius), this.y + (2 * headRadius), legRadius, 0, 2 * Math.PI);
+        }
+        ctx.fill();
+
+        //draw circles for white of the eyes
+        const eyeXOffset = this.size / 3
+        const eyeYOffset = this.size / 3
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        // left eye
+        ctx.arc(this.x + eyeXOffset, this.y + eyeYOffset, legRadius, 0, 2 * Math.PI);
+        
+        //right eye
+        ctx.arc(this.x + (2* headRadius) - eyeXOffset, this.y + eyeYOffset, legRadius, 0, 2 * Math.PI);
+        ctx.fill();
+
+        //draw circles for black of the eyes
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        // left eye
+        ctx.arc(this.x + eyeXOffset, this.y + eyeYOffset, legRadius / 2, 0, 2 * Math.PI);
+
+        //right eye
+        ctx.arc(this.x + (2* headRadius) - eyeXOffset, this.y + eyeYOffset, legRadius / 2, 0, 2 * Math.PI);
+        ctx.fill();
+
     }
 }
 
