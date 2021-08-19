2a48a1f96c6861c0eb2cb0e00490a093bdefa3fa
leoruas <leo.coelho.ruas@gmail.com>
2021-08-19 09:33:04 -0300

    open script hotfix

--- Bash/open-pacman.sh
+++ Bash/open-pacman.sh
@@ -6,9 +6,7 @@ screenNumber=0;
 for lg in $LG_FRAMES ; do
     screenNumber=${lg:2}
 	if [ $lg == "lg1" ]; then
-		export DISPLAY=:0
-        nohup chromium-browser http://localhost:$port/$screenNumber --start-fullscreen --autoplay-policy=no-user-gesture-required </dev/null >/dev/null 2>&1 &
-
+        ssh -Xnf lg@$lg " export DISPLAY=:0 ; chromium-browser http://localhost:$port/$screenNumber --start-fullscreen --autoplay-policy=no-user-gesture-required </dev/null >/dev/null 2>&1 &" || true
 	else
         ssh -Xnf lg@$lg " export DISPLAY=:0 ; chromium-browser http://lg1:$port/$screenNumber --start-fullscreen </dev/null >/dev/null 2>&1 &" || true
 	fi
